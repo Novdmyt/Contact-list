93fbb2aea0688c37a82dfcdb1cd096db3e91e2cd
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-23 18:43:59 +0300

    Add method ContactList

---
+++ src/app/list/ContactList.java
@@ -0,0 +1,15 @@
+package app.list;
+
+
+
+public class ContactList {
+    private final String name;
+    private final String phone;
+
+    public ContactList(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+
+
+}
