9e113a60abf4fa73d4e094df1323c955cd0e86e7
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-23 16:53:31 +0300

    Add method getWriteContent fix in Constants

--- src/app/Service/Write_File.java
+++ src/app/Service/Write_File.java
@@ -7,16 +7,29 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Write_File {
 
 public String getWriteFile(String fileName){
     try {
-        Path file = Path.of(Constants.BASE_PATH_IN + fileName + ".txt");
+        Path file = Path.of(Constants.BASE_PATH_IN + fileName);
         Files.createFile(file);
         return "Файл створено. ";
     }catch (IOException e) {
         return"Такий файл існує. " + e.getMessage();
       }
+    }
+
+    public String getWriteContent(ArrayList<String> list, String fileName){
+    Path file = Path.of(Constants.BASE_PATH_IN + fileName);
+    try {
+        String content = String.join("\n",list);
+        byte[] text = content.getBytes();
+        Files.write(file, text);
+        return "Данні записані у файл";
+    }catch  (IOException e) {
+        return "Помилка запису " + e.getMessage();
+    }
     }
 }
