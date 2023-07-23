fde7a67be0fff0333112d585e2cc503f84805b95
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-23 16:29:36 +0300

    Add method getWriteFile

--- src/app/Service/Write_File.java
+++ src/app/Service/Write_File.java
@@ -1,6 +1,22 @@
 package app.Service;
 
+import app.util.Constants;
+
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Write_File {
 
-
+public String getWriteFile(String fileName){
+    try {
+        Path file = Path.of(Constants.BASE_PATH_IN + fileName + ".txt");
+        Files.createFile(file);
+        return "Файл створено. ";
+    }catch (IOException e) {
+        return"Такий файл існує. " + e.getMessage();
+      }
+    }
 }
